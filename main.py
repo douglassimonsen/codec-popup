3beee32ded96dcb4adcae2261938ebb253289ab2
Matthew Hamilton <mwhamilton6@gmail.com>
2022-04-17 12:39:11 -0500

    Reactivity partially implemented

--- main.py
+++ main.py
@@ -1,49 +1,56 @@
 import tkinter as tk
 import link
-import pyperclip
 
 
 class State:
     def __init__(self) -> None:
-        self.char_index = 0
-        self.clipboard = pyperclip.paste()
+        self.char_index = 5
+        self.clipboard = root.clipboard_get()
+        self.char = tk.StringVar(root, "")
+        self.pre_string = tk.StringVar(root, "")
+        self.post_string = tk.StringVar(root, "")
+        self._update()
     
-    @property
-    def char(self):
-        return self.clipboard[self.char_index]
+    def _update(self):
+        self.char.set(self.clipboard[self.char_index])
+        self.pre_string.set(self.clipboard[:self.char_index])
+        self.post_string.set(self.clipboard[self.char_index + 1:])
 
-    @property
-    def pre_string(self):
-        return self.clipboard[:self.char_index]
+    def left_press(self, event):
+        self.char_index = max(self.char_index - 1, 0)
+        self._update()
     
-    @property
-    def post_string(self):
-        return self.clipboard[self.char_index + 1:]
+    def right_press(self, event):
+        self.char_index = min(self.char_index + 1, len(self.clipboard) - 1)
+        self._update()
 
 
+root = tk.Tk()
 state = State()
 
 
 def layout_root():
-    root = tk.Tk()
     root.geometry("400x150")
     root.grid_rowconfigure(0, weight=2)
     root.grid_rowconfigure(1, weight=10)
     root.grid_rowconfigure(2, weight=1)
     root.grid_columnconfigure(0, weight=5)
     root.grid_columnconfigure(1, weight=1)
+
+    root.bind("<KeyPress-Left>", state.left_press)
+    root.bind("<KeyPress-Right>", state.right_press)
     return root
 
 
 def layout_characters(root):
     def codepoint(byt):
-        print(byt)
-        return hex(ord(byt[0]))[2:]
+        return hex(ord(byt.get()[0]))[2:]
 
 
     def encoding(byt, encoding):
+        print(byt.get())
         try:
-            return byt.decode(encoding)
+            return byt.get().decode(encoding)
         except:
             return "Decode Error"
 
@@ -58,16 +65,16 @@ def layout_characters(root):
 
     link.Link(frame2, link=f"https://www.fileformat.info/info/unicode/char/{codepoint(state.char)}/index.htm", text="UTF-8").grid(row=1, column=1, sticky="EW")
     link.Link(frame3, link=f"https://www.i18nqa.com/debug/table-iso8859-1-vs-windows-1252.html", text="CP1252").grid(row=1, column=2, sticky="EW")
-    tk.Label(frame2, text=encoding(state.char, "UTF-8")).grid(row=0, column=1, sticky="EW")
-    tk.Label(frame3, text=encoding(state.char, "CP1252")).grid(row=0, column=2, sticky="EW")
+    tk.Label(frame2, textvariable=encoding(state.char, "UTF-8")).grid(row=0, column=1, sticky="EW")
+    tk.Label(frame3, textvariable=encoding(state.char, "CP1252")).grid(row=0, column=2, sticky="EW")
 
 
 def layout_message(root):
     frame = tk.Frame(root)
     frame.grid(row=1, column=0, sticky="nsew")
-    tk.Label(frame, text=state.pre_string).pack(side=tk.LEFT)    
-    tk.Label(frame, text=state.char, background="yellow").pack(side=tk.LEFT)    
-    tk.Label(frame, text=state.post_string).pack(side=tk.LEFT)    
+    tk.Label(frame, textvariable=state.pre_string).pack(side=tk.LEFT)    
+    tk.Label(frame, textvariable=state.char, background="yellow").pack(side=tk.LEFT)    
+    tk.Label(frame, textvariable=state.post_string).pack(side=tk.LEFT)    
 
 
 def layout_buttons(root):
